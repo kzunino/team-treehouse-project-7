ecce2881106f624b0494fb19efb6c7d7caee868a
Kyle <kzunino@gmail.com>
2019-07-29 18:53:03 -0700

    working on search route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import axios from 'axios'
 import Search from './Components/Search';
 import Navigation from './Components/Navigation'
@@ -13,6 +13,9 @@ export default class App extends Component {
     super();
     this.state = {
       photos: [],
+      catPhotos: [],
+      dogPhotos: [],
+      computerPhotos: [],
       loading: true,
       title: ''
     };
@@ -20,9 +23,28 @@ export default class App extends Component {
 
   componentDidMount(){
     this.performSearch();
+
+    this.getPhotos('cat')
+    .then(response => this.setState({
+      catPhotos: response.data.results
+    }))
+
+    this.getPhotos('dog')
+    .then(response => this.setState({
+      dogPhotos: response.data.results
+    }))
+
+    this.getPhotos('computer')
+    .then(response => this.setState({
+      computerPhotos: response.data.results
+    }))
   }
 
+
   performSearch = (query = 'home') => {
+    this.setState({
+      loading: true
+    })
     axios.get(`https://api.unsplash.com/search/photos/?page=1&per_page=16&query=${query}&client_id=${apiKey}`)
       .then(response => {
         this.setState({
@@ -36,18 +58,33 @@ export default class App extends Component {
       })
   }
 
+  getPhotos = (query) => {
+    return axios.get(`https://api.unsplash.com/search/photos/?page=1&per_page=16&query=${query}&client_id=${apiKey}`)
+        .catch(err => {
+          console.log('Something went wrong while fetching data!', err)
+        })
+      }
+
+
   render() {
     return (
       <BrowserRouter>
         <div className="container">
             <Search onSearch={this.performSearch} />
-            <Navigation onSearch={this.performSearch}/>
-          {/*            <Route path={"/"} render={(props) => <Search onSearch={this.performSearch} />} />
-                      <Route path={"/"} render={(props) => <Navigation onSearch={this.performSearch} />} />*/}
+            <Navigation />
+           {/*}<Route path={"/"} render={(props) => <Search onSearch={this.performSearch} />} />
+           <Route path={"/"} render={(props) => <Navigation onSearch={this.performSearch} />} /> */}
           {
             (this.state.loading)
             ? <p>Loading...</p>
-            : <PhotoList data={this.state.photos} title={this.state.title} />
+            :
+              <Switch>
+                <Route exact path="/" render={() =><PhotoList data={this.state.photos} title={this.state.title} />} />
+                <Route path="/cats" render={() =><PhotoList data={this.state.catPhotos} title={'Cats'} />} />
+                <Route path="/dogs" render={() =><PhotoList data={this.state.dogPhotos} title={'Dogs'} />} />
+                <Route path="/computers" render={() =><PhotoList data={this.state.computerPhotos} title={'Computers'} />} />
+                <Route path="/search/:query" render={() =><PhotoList data={this.state.photos} title={this.state.title} />} />
+              </Switch>
           }
         </div>
       </BrowserRouter>
