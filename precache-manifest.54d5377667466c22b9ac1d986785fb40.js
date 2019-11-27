922f2f5d71ad69fa2a54d92703c040a5b26d42c4
Kyle <kzunino@gmail.com>
2019-11-26 22:27:42 -0800

    Updates

---
+++ precache-manifest.54d5377667466c22b9ac1d986785fb40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "070066f45b6e0ca044241f6a6e311036",
+    "url": "/team-treehouse-project-7/index.html"
+  },
+  {
+    "revision": "77938cb3d17dbf15a953",
+    "url": "/team-treehouse-project-7/static/css/main.28430a44.chunk.css"
+  },
+  {
+    "revision": "141ead78d5cea14afe34",
+    "url": "/team-treehouse-project-7/static/js/2.38740711.chunk.js"
+  },
+  {
+    "revision": "77938cb3d17dbf15a953",
+    "url": "/team-treehouse-project-7/static/js/main.a8556e48.chunk.js"
+  },
+  {
+    "revision": "b51c37f6a99fb0cee4db",
+    "url": "/team-treehouse-project-7/static/js/runtime~main.5b9dcf4e.js"
+  }
+]);
